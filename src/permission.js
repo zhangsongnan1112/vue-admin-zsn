4fafb9522554b12596d87989137a67f38be3cca8
zhangsongnan <zsn19951112@126.com>
2020-05-27 16:11:35 +0800

    该字

--- src/permission.js
+++ src/permission.js
@@ -1,27 +1,27 @@
 import router from './router'
-// import store from './store'
-// import { getToken } from '@/utils/auth'
-// import getPageTitle from '@/utils/get-page-title'
+import store from './store'
+import { getToken } from '@/utils/auth'
+import getPageTitle from '@/utils/get-page-title'
 
 // const whiteList = ['/login']
-console.log()
-// router.beroreEach((to, form, next) => {
-// document.title = getPageTitle(to.mete.tltle)
 
-// const hasToken = getToken()
-// if (hasToken) {
-//   // hasTOken
-//   if (to.path === '/login') {
-//     next({path: '/'})
-//   } else {
-//     const hasUserInfo = store.getters.name
-//     if (hasUserInfo) {
-//       next()
-//     } else {
+router.beforeEach((to, from, next) => {
+  document.title = getPageTitle(to.meta.title)
 
-//     }
-//   }
-// } else {
-//   /* no Token */
-// }
-// })
+  const hasToken = getToken()
+  if (hasToken) {
+  // hasTOken
+    if (to.path === '/login') {
+      next({path: '/'})
+    } else {
+      const hasUserInfo = store.getters.name
+      if (hasUserInfo) {
+        next()
+      } else {
+
+      }
+    }
+  } else {
+  /* no Token */
+  }
+})
