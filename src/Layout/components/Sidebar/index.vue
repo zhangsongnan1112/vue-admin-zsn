e858a4769c16a6979a726b875113f669e3be0458
zhangsongnan <zsn19951112@126.com>
2020-07-02 16:30:17 +0800

    增加el-scroll

--- src/Layout/components/Sidebar/index.vue
+++ src/Layout/components/Sidebar/index.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="sidebar-container">
-    <el-menu
-     :default-active="activeMenu"
-      mode="vertical"
-      :collapse="isCollapse"
-      :background-color="variables.menuBg"
-      :text-color="variables.menuText"
-      :active-text-color="variables.$menuActiveText"
-    >
-      <Sidebar  v-for="item in permission_routes" :key = item.path :item="item" :base-path="item.path"> </Sidebar>
-    </el-menu>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+      :default-active="activeMenu"
+        mode="vertical"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :active-text-color="variables.$menuActiveText"
+      >
+        <Sidebar  v-for="item in permission_routes" :key = item.path :item="item" :base-path="item.path"> </Sidebar>
+      </el-menu>
+    </el-scrollbar>
   </div>
 </template>
 <script>
