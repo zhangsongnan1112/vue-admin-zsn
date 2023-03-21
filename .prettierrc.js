71127d11a1992d6d09b3f5c6c5c93015c3cbd497
zhangsn <songnan.zhang@ishansong.com>
2023-03-21 14:15:52 +0800

    解决报错zsn

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  // 使用tab还是空格
+  useTabs: false,
+  singleAttributePerLine: true
+}
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,25 +1,51 @@
 <template>
-  <div>{{obj}}
+  <div>
+    <!-- {{ obj }}
     <button @click="change">11</button>
-    <button @click="change1">11</button>
+    <el-button @click="change1">11</el-button> -->
+    <el-date-picker
+      v-model="searchWeek"
+      type="week"
+      format="yyyy 第 WW 周"
+      placeholder="选择周"
+    >
+    </el-date-picker>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  name: 'dashboard',
+  data() {
     return {
       obj: {
         a: 'a'
+      },
+      searchWeek: '',
+      weekDateOption: {
+        firstDayOfWeek: 1,
+        disabledDate(time) {
+          const startTime = new Date('2019-12-30').getTime()
+          const today = new Date()
+          const d = today.getDay()
+          let w = 0
+          if (d === 0) {
+            w = 7
+          } else {
+            w = d
+          }
+          const endTime = today.setDate(today.getDate() - w)
+          return time.getTime() > endTime || time.getTime() < startTime
+        }
       }
     }
   },
   methods: {
-    change () {
+    change() {
       this.obj.a = 1
       console.log(this.obj)
     },
-    change1 () {
+    change1() {
       // this.obj.b = 2
       console.log(this.obj)
     }
@@ -27,6 +53,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
